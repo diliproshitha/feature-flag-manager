5e86f6a5e21d857665354b3db2e4c500f0596e27
Dilip Roshitha <dilip.roshitha@grubtech.com>
2024-02-03 10:03:56 +0530

    added feature-flag-manager-service

---
+++ feature-flag-manager-service/src/main/java/com/dilip/platform/featureflagmanagerservice/FeatureFlagManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dilip.platform.featureflagmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagManagerServiceApplication.class, args);
+	}
+
+}
---
+++ feature-flag-manager-service/src/test/java/com/dilip/platform/featureflagmanagerservice/FeatureFlagManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilip.platform.featureflagmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
