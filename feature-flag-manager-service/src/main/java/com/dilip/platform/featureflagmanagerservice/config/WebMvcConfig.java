9114578c5c42409a92a047d03fb678ac895ac525
Dilip Roshitha <dilip.roshitha@grubtech.com>
2024-02-06 11:00:48 +0530

    added CORS configs

---
+++ feature-flag-manager-service/src/main/java/com/dilip/platform/featureflagmanagerservice/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.dilip.platform.featureflagmanagerservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(final CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
+  }
+
+}
